6411f953efe148e0ed13b9c423ff8166542f1c99
HasanSan <145437224+HasyanGsan@users.noreply.github.com>
2024-10-27 17:18:08 +0300

    Добавил проверку на интернет и сообщение о его отсутствии

--- app/src/main/java/com/example/casehub/main/MainActivity.kt
+++ app/src/main/java/com/example/casehub/main/MainActivity.kt
@@ -16,19 +16,29 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.casehub.R
 import com.example.casehub.databinding.ActivityMainBinding
+import com.example.network.ConnectivityObserver
+import com.example.network.NetworkConnectivityObserver
 import com.google.android.material.appbar.MaterialToolbar
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+
+    //Необходимые данные для проверки интернета
+    private lateinit var networkBlank: ConstraintLayout
+    private lateinit var connectivityObserver: ConnectivityObserver
 
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,6 +49,8 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+           networkBlank = findViewById(R.id.network_include)
+                connectivityObserver = NetworkConnectivityObserver(applicationContext)
         val bottomNavBar = binding.bottomNavigationMain
         val navController = findNavController(R.id.navHostFragment)
         bottomNavBar.setupWithNavController(navController)
@@ -56,6 +68,32 @@ class MainActivity : AppCompatActivity() {
             it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
             it.hide(WindowInsets.Type.systemBars())
         }
+
+        //Проверка интернета после запуска
+        val currentStatus =  (connectivityObserver as NetworkConnectivityObserver)  .getCurrentStatus()
+        networkBlank.visibility = if(currentStatus == ConnectivityObserver.Status.Available){
+            ConstraintLayout.GONE
+        }
+        else{
+            ConstraintLayout.VISIBLE
+        }
+
+        //Ассинхронная проверка интернета
+        lifecycleScope.launch {
+            connectivityObserver.observe().collect { status ->
+                when(status){
+                    ConnectivityObserver.Status.Available -> {
+                        networkBlank.visibility = ConstraintLayout.GONE
+                    }
+                    else -> {
+                        networkBlank.visibility = ConstraintLayout.VISIBLE
+                    }
+                }
+
+            }
+        }
+
+        TODO("Пофикси возможность свапа темы и взаимодействие с поисковиком в случае потери интернета.");
 
 
 //        binding.bottomNavigationBar.setOnApplyWindowInsetsListener(null)
---
+++ app/src/main/java/com/example/network/NetworkConnectivityObserver.kt
@@ -0,0 +1,78 @@
+package com.example.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+
+//Проверка на наличие интернета
+class NetworkConnectivityObserver(
+    private val context: Context
+) : ConnectivityObserver {
+    private val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    override fun observe(): Flow<ConnectivityObserver.Status> {
+        return callbackFlow {
+            val callback = object : ConnectivityManager.NetworkCallback() {
+                override fun onUnavailable() {
+                    super.onUnavailable()
+                    launch {
+                        send(ConnectivityObserver.Status.Unavailable)
+                    }
+                }
+
+                override fun onLost(network: Network) {
+                    super.onLost(network)
+                    launch {
+                        send(ConnectivityObserver.Status.Lost)
+                    }
+                }
+
+                override fun onLosing(network: Network, maxMsToLive: Int) {
+                    super.onLosing(network, maxMsToLive)
+                    launch {
+                        send(ConnectivityObserver.Status.Losing)
+                    }
+                }
+
+                override fun onAvailable(network: Network) {
+                    super.onAvailable(network)
+                    launch {
+                        send(ConnectivityObserver.Status.Available)
+                    }
+                }
+            }
+            connectivityManager.registerDefaultNetworkCallback(callback)
+            awaitClose {
+                connectivityManager.unregisterNetworkCallback(callback)
+            }
+        }.distinctUntilChanged()
+    }
+
+    fun getCurrentStatus(): ConnectivityObserver.Status {
+        return try {
+            val network = connectivityManager.activeNetwork
+            if (network != null && connectivityManager.getNetworkCapabilities(network)?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true) {
+                ConnectivityObserver.Status.Available
+            } else {
+                ConnectivityObserver.Status.Unavailable
+            }
+        } catch (e: Exception) {
+            ConnectivityObserver.Status.Unavailable
+        }
+    }
+}
+
+interface ConnectivityObserver {
+    fun observe(): Flow<Status>
+
+    enum class Status {
+        Available, Unavailable, Losing, Lost //Состояния сети
+    }
+}
